025029e1ef4c60fa214e232721e130671f2de347
yonas <yonas2754@gmail.com>
2025-01-20 17:09:10 -0800

    fond problemcommit

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -11,9 +11,9 @@ async function Page() {
     <div className='bg-ethBlack-600  flex  items-center justify-center '>
      
     <div className=' text-white w-full h-full min-h-screen font-bold flex flex-col  mb-60'>
-    {session?.user && (
+   {/*  {session?.user && (
         <div>{JSON.stringify(session.user)}</div>
-      )}
+      )} */}
      
     <div className='flex flex-col rounded-t-[48px] bg-ethBlack-500 border-t-2 shadow-2xl border-ethYellow-500 w-full grow p-8'>
       
